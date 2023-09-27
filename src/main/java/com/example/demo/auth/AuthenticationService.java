78a9ec044d725099ff22d59c62015729f69958d2
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-09-27 17:54:44 +0900

    AuthenticationService#register

--- src/main/java/com/example/demo/auth/AuthenticationService.java
+++ src/main/java/com/example/demo/auth/AuthenticationService.java
@@ -1,14 +1,35 @@
 package com.example.demo.auth;
 
 
+import com.example.demo.config.JwtService;
+import com.example.demo.user.Role;
+import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
