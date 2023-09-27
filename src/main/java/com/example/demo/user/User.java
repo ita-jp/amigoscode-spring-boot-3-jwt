e0731ccd4ff6d097cbe298aa9974e4378cdc502c
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-09-27 16:12:20 +0900

    table

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -1,6 +1,10 @@
 package com.example.demo.user;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +14,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     private String firstname;
     private String lastname;
