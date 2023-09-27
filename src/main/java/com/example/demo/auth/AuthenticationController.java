d96dc002fbd35bd77daa43898442b6567b1a7197
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-09-27 17:50:02 +0900

    AuthenticationService

--- src/main/java/com/example/demo/auth/AuthenticationController.java
+++ src/main/java/com/example/demo/auth/AuthenticationController.java
@@ -12,18 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final AuthenticationService authenticationService;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
-        return null;
+        return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody AuthenticationRequest request
     ) {
-        return null;
+        return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 
 }
---
+++ src/main/java/com/example/demo/auth/AuthenticationService.java
@@ -0,0 +1,17 @@
+package com.example.demo.auth;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    public AuthenticationResponse register(RegisterRequest request) {
+        return null;
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
