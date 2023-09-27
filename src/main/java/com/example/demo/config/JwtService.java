067d38163a7b15603f5bc263dfb028ed4c3cef1c
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-09-27 16:51:07 +0900

    extractUsername

--- src/main/java/com/example/demo/config/JwtService.java
+++ src/main/java/com/example/demo/config/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,7 +15,12 @@ public class JwtService {
     private static final String SECRET_KEY = "292915fd1b20a3ca268ec175748954ea3c49d39f120222645a13f3b881029b42";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        var claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
